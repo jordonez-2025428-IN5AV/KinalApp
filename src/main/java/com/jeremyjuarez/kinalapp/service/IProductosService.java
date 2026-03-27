682b6933ad85c59895658c0930d685260269a710
jordonez-2025428 <jordonez-2025428@kinal.edu.gt>
2026-03-27 10:37:35 -0600

    feat: creación de la capa service de Producto por jordonez-2025428

--- src/main/java/com/jeremyjuarez/kinalapp/service/IProductosService.java
+++ src/main/java/com/jeremyjuarez/kinalapp/service/IProductosService.java
@@ -1,7 +1,6 @@
 package com.jeremyjuarez.kinalapp.service;
 
 import com.jeremyjuarez.kinalapp.entity.Productos;
-import com.jeremyjuarez.kinalapp.entity.Usuario;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,10 +9,10 @@ public interface IProductosService {
     List<Productos> listarUsuarios();
     List<Productos> listarActivos();
 
-    Usuario guardar(Usuario usuario);
+    Productos guardar(Productos productos);
 
     Optional<Productos> buscarPorCodigoP (String codigoP);
-    Productos actualizar(String codigoP, Usuario usuario);
+    Productos actualizar(String codigoP, Productos productos);
     void eliminar(String codigo);
     boolean existerPorCodigo (String codigo);
 }
